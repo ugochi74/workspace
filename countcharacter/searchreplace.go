ac8df40f834ed03e051ebc3c03558022e337e514
ugochi74 <confidenceodo4@gmail.com>
2025-10-30 00:53:20 +0100

    Create searchreplace.go

---
+++ countcharacter/searchreplace.go
@@ -0,0 +1,22 @@
+func main() {
+    if len(os.Args) != 4 {
+        return
+    }
+    str := os.Args[1]
+    old := os.Args[2]
+    new := os.Args[3]
+
+    if len(old) != 1 || len(New) {
+        return
+    }
+    oldRune := [] rune (old) [0]
+    NewRune := [] rune (new) [0]
+    for _, ch := range str {
+        if ch == oldRune {
+        }   
+        z01.printRune (NewRune)
+    } else {
+        z01.PrintRune ()
+    }
+        z01.PrintRune('\n')
+}
