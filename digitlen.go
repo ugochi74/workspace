64df7419e6a27f3d7b41568b565a41df60de1572
ugochi74 <confidenceodo4@gmail.com>
2025-10-29 23:49:33 +0100

    Create digitlen.go

---
+++ digitlen.go
@@ -0,0 +1,17 @@
+package piscine
+
+func DigitLen(n, base int) int {
+    if base < 2 || base > 36 {
+        return -1
+    }
+      if n < 0 {
+        return -n
+      }
+      count := 0
+      for n > 0 {
+        n = n/base
+        count++
+      }
+       return count
+}      
+      
