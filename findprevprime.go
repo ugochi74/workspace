af70fc2efe5bf5353e040d0ed1a2e5f7c70fdf4e
ugochi74 <confidenceodo4@gmail.com>
2025-12-02 23:56:27 +0100

    Implement function to find previous prime number

---
+++ findprevprime.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func FindPrevPrime(nb int) int {
+	if nb < 2 {
+		return 0
+	}
+	for i := 2; i*i <= nb; i++ {
+		if nb%i == 0 {
+			return FindPrevPrime(nb - 1)
+		}
+	}
+	return nb
+}
+
+func main() {
+	fmt.Println(FindPrevPrime(5))
+	fmt.Println(FindPrevPrime(4))
+}
